c4104399fa92aec8efc139246cc8861c32016a10
Jerome Brown <Jerome@SoliDeoGloria.tech>
2026-07-08 15:47:02 +1200

    fix: impossible string captures

--- bicep/queries/highlights.scm
+++ bicep/queries/highlights.scm
@@ -30,16 +30,10 @@
   name: (identifier) @module)
 
 (extension_statement
-  name: (string) @string.special.path)
-
-(extension_statement
   alias: (identifier) @module)
 
 (extension_with_statement
   name: (identifier) @module)
-
-(extension_with_statement
-  name: (string) @string.special.path)
 
 (extension_with_statement
   alias: (identifier) @module)
